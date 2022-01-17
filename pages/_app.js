783e61a7b209b1804929b36b634ce4368e2838ce
HenriFourie <61292359+henrifourieIO@users.noreply.github.com>
2022-01-17 11:08:01 +0200

    fixed tracking code

--- pages/_app.js
+++ pages/_app.js
@@ -10,6 +10,21 @@ function MyApp({ Component, pageProps }) {
     <>
       <Head>
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossOrigin="anonymous" />
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=UA-167450114-1"
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'UA-167450114-1');
+        `,
+          }}
+        ></script>
       </Head>
       <Nav />
       <Component {...pageProps} />
